a5f7e4bb05432e369e6edf825114b6aa7516958f
Tran Phu Huong <tranphuhuong1802@gmail.com>
2026-01-05 19:43:20 +0700

    up

--- lesson1/typescript.js
+++ lesson1/typescript.js
@@ -2,6 +2,8 @@ console.log("javascript");
 console.log("huongtp");
 console.log("javascript");
 console.log("huongtp");
+console.log("huongtp");
+console.log("huongtp");
 
 // let myName: string = "huongtp";
 // console.log(myName);
